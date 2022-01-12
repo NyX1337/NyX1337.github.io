114dd3ee311d3992a04d3c2e0e5aef2d86627f2c
NyX1337 <bdnov123@hotmail.com>
2022-01-12 15:08:09 +0100

    added localstorage to store data

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function SendUpit() {
   localStorage.setItem('krevet1', 'idegas')
-  localStorage.setItem('krevet2', 'idegas2')
-  localStorage.setItem('krevet3', 'idegas3')
+  // localStorage.setItem('krevet2', 'idegas2')
+  // localStorage.setItem('krevet3', 'idegas3')
 }
 document.addEventListener('DOMContentLoaded', SendUpit)
